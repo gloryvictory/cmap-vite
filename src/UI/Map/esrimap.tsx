773a330adf50750d325eb9240271c8d99fe8eb32
gloryvictory <zamaraev@gmail.com>
2023-01-31 09:05:58 +0500

    add map

---
+++ src/UI/Map/esrimap.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function ESRIMap() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="ESRIMap">
+      <h1> HELLO !!!</h1>
+    </div>
+  );
+}
+
+export default ESRIMap;
