879c1eb70a5b70227757385a5be6775e9bb0af5d
gloryvictory <zamaraev@gmail.com>
2023-02-03 13:14:43 +0500

    2 files modified, 1 added

---
+++ src/UI/Header/Header.tsx
@@ -0,0 +1,16 @@
+import '@esri/calcite-components/dist/calcite/calcite.css';
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+
+import { useState } from 'react';
+
+function Header() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="ESRIMap">
+      <h1> </h1>
+    </div>
+  );
+}
+
+export default Header;
--- src/UI/Map/esrimap.tsx
+++ src/UI/Map/esrimap.tsx
@@ -1,12 +1,44 @@
+import MapView from 'esri/views/MapView';
+import WebMap from 'esri/WebMap';
+import Legend from 'esri/widgets/Legend';
+import ScaleBar from 'esri/widgets/ScaleBar';
 import { useState } from 'react';
+// import { calcite-shell, calcite-shell-panel,  calcite-action-bar, calcite-action} from "@esri/calcite-components";
 
 function ESRIMap() {
   // const [count, setCount] = useState(0)
 
+  // esriConfig.apiKey = "YOUR_API_KEY";
+
+  const webmap = new WebMap({
+    portalItem: {
+      id: '41281c51f9de45edaf1c8ed44bb10e30',
+    },
+  });
+
+  const view = new MapView({
+    container: 'viewDiv',
+    map: webmap,
+  });
+
+  const scalebar = new ScaleBar({
+    view: view,
+  });
+
+  view.ui.add(scalebar, 'bottom-left');
+
+  const legend = new Legend({
+    view: view,
+  });
+  view.ui.add(legend, 'top-right');
+
   return (
-    <div className="ESRIMap">
-      <h1> HELLO !!!</h1>
-    </div>
+    <>
+      <div className="ESRIMap">
+        <h1> HELLO !!!</h1>
+        {view}
+      </div>
+    </>
   );
 }
 
